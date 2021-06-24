b0127dac51d5f12d5fd0f70864efd0fc41416925
AmarsDing <315150726@qq.com>
2021-06-24 10:37:21 +0800

    1

--- os/ycache/ycache_adapter_memory_item.go
+++ os/ycache/ycache_adapter_memory_item.go
@@ -1,14 +1,14 @@
 package ycache
 
 import (
-	"github.com/AmarsDing/lib/os/gtime"
+	"github.com/AmarsDing/lib/os/ytime"
 )
 
 // IsExpired checks whether <item> is expired.
 func (item *adapterMemoryItem) IsExpired() bool {
 	// Note that it should use greater than or equal judgement here
 	// imagining that the cache time is only 1 millisecond.
-	if item.e >= gtime.TimestampMilli() {
+	if item.e >= ytime.TimestampMilli() {
 		return false
 	}
 	return true
